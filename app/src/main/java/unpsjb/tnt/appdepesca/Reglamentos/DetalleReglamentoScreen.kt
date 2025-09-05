dc5636bb9ec79ce4fbc29da5f081b591e42cf3d7
FrancoDeMarco <franco92tw@gmail.com>
2025-09-05 10:31:09 -0300

    Dejo el DetalleReglamento hasta ahí.

--- app/src/main/java/unpsjb/tnt/appdepesca/Reglamentos/DetalleReglamentoScreen.kt
+++ app/src/main/java/unpsjb/tnt/appdepesca/Reglamentos/DetalleReglamentoScreen.kt
@@ -4,17 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -36,9 +30,7 @@ fun DetalleReglamentoScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF1B2B24))
-            .padding(24.dp),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.Start
+            .padding(16.dp),
     ) {
         ///// REGLAMENTO
         Text(
@@ -47,6 +39,8 @@ fun DetalleReglamentoScreen(
             fontWeight = FontWeight.Bold,
             color = Color(0xFF3E8B75),
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
 
         ///// LUGAR
         Text(
@@ -61,6 +55,8 @@ fun DetalleReglamentoScreen(
             fontSize = 18.sp,
             color = Color.White
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         ////DESCRIPCIÓN
         Column {
@@ -77,6 +73,8 @@ fun DetalleReglamentoScreen(
                 color = Color.White
             )
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         ////ENLACES
         Column {
@@ -96,6 +94,7 @@ fun DetalleReglamentoScreen(
         }
 
         /////BOTÓN DE RETROCESO
+        Spacer(modifier = Modifier.weight(1f))
         Button(
             onClick = { navController.popBackStack() },
             modifier = Modifier
