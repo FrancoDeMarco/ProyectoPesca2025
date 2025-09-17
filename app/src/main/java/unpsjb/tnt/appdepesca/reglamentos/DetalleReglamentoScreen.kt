7c91d7217c4769aa8515493d4681278ffa0b1c52
FrancoDeMarco <franco92tw@gmail.com>
2025-09-17 10:18:36 -0300

    Botón de Volver de DetalleReglamento está alineado.

--- app/src/main/java/unpsjb/tnt/appdepesca/reglamentos/DetalleReglamentoScreen.kt
+++ app/src/main/java/unpsjb/tnt/appdepesca/reglamentos/DetalleReglamentoScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -29,83 +30,98 @@ fun DetalleReglamentoScreen(
     Spacer(modifier = Modifier.height(16.dp))
 
     val reglamento = viewModel.reglamentos.find { it.reglamentoId == reglamentoId }
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF1B2B24))
-            .padding(16.dp),
+            .background(color = Color(0xFF1B2B24))
     ) {
-        ///// REGLAMENTO
-        Text(
-            text = "${reglamento?.reglamentoNombre}",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF3E8B75),
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        ///// LUGAR
-        Text(
-            "${reglamento?.reglamentoLugar}",
-            fontSize = 24.sp,
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        /////FECHA
-        Text(
-            text = "${reglamento?.relgamentoFecha}",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF3E8B75),
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        ////DESCRIPCIÓN
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF1B2B24))
+        ) {
+            ///// REGLAMENTO
             Text(
-                text = "${reglamento?.reglamentoDescripcion}",
+                text = "${reglamento?.reglamentoNombre}",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF3E8B75),
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            ///// LUGAR
+            Text(
+                "${reglamento?.reglamentoLugar}",
+                fontSize = 24.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            /////FECHA
+            Text(
+                text = "${reglamento?.relgamentoFecha}",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color.White
+                color = Color(0xFF3E8B75),
             )
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        ////ENLACES
-        Column {
-            reglamento?.reglamentoEnlaces?.forEach { enlace ->
+            ////DESCRIPCIÓN
+            Column {
                 Text(
-                    text = enlace,
-                    color = Color(0xFF3E8B75),
+                    text = "${reglamento?.reglamentoDescripcion}",
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
+                    color = Color.White
                 )
             }
-        }
 
-        /////BOTÓN DE RETROCESO
-        Spacer(modifier = Modifier.weight(1f))
-        Button(
-            onClick = { navController.popBackStack() },
-            modifier = Modifier
-                .size(88.dp) // fuerza cuadrado perfecto
-                .offset(x = 8.dp, y = (-15).dp) // manejar corrimiento del botón
-                .border(2.dp, Color(0xFF3E8B75), RectangleShape),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1B2B24)),
-            shape = RectangleShape, // <- esto lo hace cuadrado
-            contentPadding = PaddingValues(0.dp) // quita el padding interno por defecto
-        ) {
-            Image(
-                painter = painterResource(R.drawable.retroceso), //nombre de la imagen
-                contentDescription = "Retroceso",
-                modifier = Modifier.size(50.dp) // tamaño de la imagen dentro del botón
-            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ////ENLACES
+            Column {
+                reglamento?.reglamentoEnlaces?.forEach { enlace ->
+                    Text(
+                        text = enlace,
+                        color = Color(0xFF3E8B75),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
         }
+        BotonVolver(
+            navController,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .offset(x = 24.dp, y = (-32).dp)
+        )
+    }
+}
+
+/////BOTÓN DE RETROCESO
+@Composable
+fun BotonVolver(
+    navController: NavController,
+    modifier: Modifier = Modifier) {
+    Button(
+        onClick = { navController.popBackStack() },
+        modifier = modifier
+            .size(88.dp) // fuerza cuadrado perfecto
+            .border(2.dp, Color(0xFF3E8B75), RectangleShape),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1B2B24)),
+        shape = RectangleShape, // <- esto lo hace cuadrado
+        contentPadding = PaddingValues(0.dp) // quita el padding interno por defecto
+    ) {
+        Image(
+            painter = painterResource(R.drawable.retroceso), //nombre de la imagen
+            contentDescription = "Retroceso",
+            modifier = Modifier.size(50.dp) // tamaño de la imagen dentro del botón
+        )
     }
 }
