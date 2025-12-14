ec78a3f53effeb5f4df203f67efd437ffe61ccf9
FrancoDeMarco <franco92tw@gmail.com>
2025-12-14 11:53:38 -0300

    Adaptando el Dropdown Menu al Compose nuevo.

--- app/src/main/java/unpsjb/tnt/appdepesca/eventos/ListadoEventosScreen.kt
+++ app/src/main/java/unpsjb/tnt/appdepesca/eventos/ListadoEventosScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -131,7 +132,10 @@ fun FiltroEventosDropdown(
                 ExposedDropdownMenuDefaults.TrailingIcon(expanded)
             },
             modifier = Modifier
-                .menuAnchor()
+                .menuAnchor(
+                    type = MenuAnchorType.PrimaryNotEditable,
+                    enabled = true
+                )
                 .fillMaxWidth()
         )
 
